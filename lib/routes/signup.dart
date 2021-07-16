0b4bbe645010778dc359cec816e610a9dd8b8d4c
Baris Kerem Kurt <kkurt@sabanciuniv.edu>
2021-07-16 16:59:29 +0300

    sign up completed

--- lib/routes/signup.dart
+++ lib/routes/signup.dart
@@ -1,4 +1,6 @@
+import 'package:cs310_week5_app/utils/dimension.dart';
 import 'package:cs310_week5_app/utils/styles.dart';
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:cs310_week5_app/utils/color.dart';
 
@@ -8,6 +10,37 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+  int attemptCount;
+  String mail;
+  String pass;
+  String pass2;
+  String userName;
+  final _formKey = GlobalKey<FormState>();
+
+  Future<void> showDialogBox(String title, String message) async {
+    return showDialog<void>(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(title),
+            content: SingleChildScrollView(
+              child: ListBody(children: [
+                Text(message),
+              ]),
+            ),
+            actions: [
+              TextButton(
+                child: Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +52,200 @@ class _SignUpState extends State<SignUp> {
         backgroundColor: AppColors.primary,
         centerTitle: true,
         elevation: 0.0,
+      ),
+      body: Padding(
+        padding: Dimen.regularPadding,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            fillColor: AppColors.secondary,
+                            filled: true,
+                            hintText: 'E-mail',
+                            //labelText: 'Username',
+                            labelStyle: kLabelLightTextStyle,
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(color: AppColors.primary),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(30.0)),
+                            ),
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter your e-mail';
+                            }
+                            if (!EmailValidator.validate(value)) {
+                              return 'The e-mail address is not valid';
+                            }
+                            return null;
+                          },
+                          onSaved: (String value) {
+                            mail = value;
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16.0),
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            fillColor: AppColors.secondary,
+                            filled: true,
+                            hintText: 'Username',
+                            //labelText: 'Username',
+                            labelStyle: kLabelLightTextStyle,
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(color: AppColors.primary),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(30.0)),
+                            ),
+                          ),
+                          keyboardType: TextInputType.text,
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter your username';
+                            }
+                            if (value.length < 4) {
+                              return 'username is too short';
+                            }
+                            return null;
+                          },
+                          onSaved: (String value) {
+                            userName = value;
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16.0),
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            fillColor: AppColors.secondary,
+                            filled: true,
+                            hintText: 'Password',
+                            //labelText: 'Username',
+                            labelStyle: kLabelLightTextStyle,
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(color: AppColors.primary),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(30.0)),
+                            ),
+                          ),
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
+                          enableSuggestions: false,
+                          autocorrect: false,
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter your password';
+                            }
+                            if (value.length < 8) {
+                              return 'Password must be at least 8 characters';
+                            }
+                            return null;
+                          },
+                          onSaved: (String value) {
+                            pass = value;
+                          },
+                        ),
+                      ),
+                      SizedBox(width: 8.0),
+                      Expanded(
+                        flex: 1,
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            fillColor: AppColors.secondary,
+                            filled: true,
+                            hintText: 'Password (Repeat)',
+                            //labelText: 'Username',
+                            labelStyle: kLabelLightTextStyle,
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(color: AppColors.primary),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(30.0)),
+                            ),
+                          ),
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
+                          enableSuggestions: false,
+                          autocorrect: false,
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter your password';
+                            }
+                            if (value.length < 8) {
+                              return 'Password must be at least 8 characters';
+                            }
+                            return null;
+                          },
+                          onSaved: (String value) {
+                            pass2 = value;
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16.0),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Expanded(
+                        flex: 1,
+                        child: OutlinedButton(
+                          onPressed: () {
+                            if (_formKey.currentState.validate()) {
+                              _formKey.currentState.save();
+                              if (pass != pass2) {
+                                showDialogBox('Error', 'Passwords must match');
+                              } else {
+                                //TODO: Sign up process
+                              }
+                              setState(() {
+                                attemptCount++;
+                              });
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(SnackBar(
+                                content: Text('Signing in...'),
+                              ));
+                            }
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 12.0),
+                            child: Text(
+                              'Sign Up',
+                              style: kButtonDarkTextStyle,
+                            ),
+                          ),
+                          style: OutlinedButton.styleFrom(
+                            backgroundColor: AppColors.primary,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
