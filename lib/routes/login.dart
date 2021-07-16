fd286e964cd05e730ea77be7332fc655de4815a3
Baris Kerem Kurt <kkurt@sabanciuniv.edu>
2021-07-16 15:22:36 +0300

    1 30 00

--- lib/routes/login.dart
+++ lib/routes/login.dart
@@ -2,6 +2,7 @@ import 'package:cs310_week5_app/utils/color.dart';
 import 'package:cs310_week5_app/utils/dimension.dart';
 import 'package:cs310_week5_app/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -11,12 +12,36 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   int attemptCount;
 
+  Future<void> showDialogBox(String title, String message) async {
+    return showDialog<void>(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(title),
+            content: SingleChildScrollView(
+              child: ListBody(children: [
+                Text(message),
+              ]),
+            ),
+            actions: [
+              TextButton(
+                child: Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        });
+  }
+
   void getData() async {
-    String name = await Future.delayed(Duration(seconds: 5), () {
+    String name = await Future.delayed(Duration(seconds: 3), () {
       return 'B. Kerem KURT';
     });
 
-    String uni = await Future.delayed(Duration(seconds: 2), () {
+    String uni = await Future.delayed(Duration(seconds: 1), () {
       return 'Sabanci University';
     });
 
@@ -46,28 +71,45 @@ class _LoginState extends State<Login> {
       ),
       body: Padding(
         padding: Dimen.regularPadding,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Expanded(
-              flex: 1,
-              child: OutlinedButton(
-                onPressed: () {
-                  setState(() {
-                    attemptCount++;
-                  });
-                },
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 12.0),
-                  child: Text(
-                    'Login Attempt: $attemptCount',
-                    style: kButtonDarkTextStyle,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'You have pushed the button this many times:',
+              style: GoogleFonts.oswald(
+                  textStyle: Theme.of(context).textTheme.headline4),
+            ),
+            Text(
+              '$attemptCount',
+              style: GoogleFonts.lato(
+                  fontStyle: FontStyle.italic,
+                  textStyle: Theme.of(context).textTheme.headline4),
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: OutlinedButton(
+                    onPressed: () {
+                      showDialogBox('Action', 'Button Clicked');
+                      setState(() {
+                        attemptCount++;
+                      });
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 12.0),
+                      child: Text(
+                        'Login Attempt: $attemptCount',
+                        style: kButtonDarkTextStyle,
+                      ),
+                    ),
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: AppColors.primary,
+                    ),
                   ),
                 ),
-                style: OutlinedButton.styleFrom(
-                  backgroundColor: AppColors.primary,
-                ),
-              ),
+              ],
             ),
           ],
         ),
